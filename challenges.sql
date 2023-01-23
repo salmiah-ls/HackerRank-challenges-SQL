e5e3a7c0f059852911aba60bd9b2874bf510eced
Sitti Salmiah <69421787+salmiah-ls@users.noreply.github.com>
2023-01-24 02:06:30 +0800

    Add files via upload

---
+++ challenges.sql
@@ -0,0 +1,14 @@
+-- Challenge 1: Revising the Select Query I
+-- https://www.hackerrank.com/challenges/revising-the-select-query/problem
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
+
+
+-- Challenge 2: Revising the Select Query II
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000
