73f83dfce9e614019e1ff6edb0e3f6bf97d839e0
Sitti Salmiah <69421787+salmiah-ls@users.noreply.github.com>
2023-01-25 11:50:29 +0800

    Update basic_join.sql

--- basic_join.sql
+++ basic_join.sql
@@ -6,3 +6,22 @@ from CITY
 inner join COUNTRY
 on CITY.COUNTRYCODE = COUNTRY.CODE
 and CONTINENT = 'Asia'
+
+
+-- Challenge 2: African Cities
+-- https://www.hackerrank.com/challenges/african-cities/problem
+
+select CITY.NAME
+from CITY
+inner join COUNTRY
+on CITY.COUNTRYCODE = COUNTRY.CODE
+and CONTINENT = 'Africa'
+
+
+-- Challenge 3: Average Population of Each Continent
+-- https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
+
+select COUNTRY.CONTINENT, ROUND(AVG(CITY.POPULATION), 0)
+from COUNTRY
+inner join CITY on COUNTRY.CODE = CITY.COUNTRYCODE
+group by COUNTRY.CONTINENT
